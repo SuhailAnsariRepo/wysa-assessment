9ffc019e912183fe44637b134b3789b8772bb34b
Suhail Ansari <suhail.ansari@wiom.in>
2023-10-29 01:32:08 +0530

    connected server to database

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+})
+.catch(error => {
+  console.error('Error connecting to the database: ', error);
+});
+
+app.use(express.json());
+app.use(cors());
